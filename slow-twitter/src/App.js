692525500849168b97e146a9b76a646b28774da3
isaacsheets <icsheets97@gmail.com>
2019-08-14 15:45:23 -0600

    auth tutorial intial integration

--- slow-twitter/src/App.js
+++ slow-twitter/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TwitterLogin from 'react-twitter-auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { isAuthenticated: false, user: null, token: '' };
+  }
+
+  onSuccess = (response) => {
+    const token = response.headers.get('x-auth-token');
+    response.json().then(user => {
+      if (token) {
+        this.setState({ isAuthenticated: true, user: user, token: token });
+      }
+    });
+  };
+
+  onFailed = (error) => {
+    alert(error);
+  };
+
+  logout = () => {
+    this.setState({ isAuthenticated: false, token: '', user: null })
+  };
+  render() {
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button" >
+              Log out
+            </button>
+          </div>
+        </div>
+      ) :
+      (
+        <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
+          onFailure={this.onFailed} onSuccess={this.onSuccess}
+          requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse" />
+      );
+
+    return (
+      <div className="App">
+        {content}
+      </div>
+    );
+  }
 }
 
 export default App;
